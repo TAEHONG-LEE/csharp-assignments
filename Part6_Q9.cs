f752c94d5e6ff00d9f1eb55a260affea8a1e49ae
ekfzha <151455218+ekfzha@users.noreply.github.com>
2025-05-01 19:20:41 +0900

    Update Part6_Q9.cs

--- Part6_Q9.cs
+++ Part6_Q9.cs
@@ -1,50 +1,51 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace ConsoleApp2
-//{
+namespace ConsoleApp2
+{
+    //Part6_Q9
 
-//    class Animal
-//    {
-//        public virtual void Sound()
-//        {
-//            Console.WriteLine("동물소리");
+    class Animal
+    {
+        public virtual void Sound()
+        {
+            Console.WriteLine("동물소리");
 
-//        }
-//    }
+        }
+    }
 
-//    class Dog : Animal
-//    {
-//        public override void Sound()
-//        {
-//            Console.WriteLine("멍멍");
-//        }
-//    }
+    class Dog : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("멍멍");
+        }
+    }
 
-//    class Cat : Animal
-//    {
-//        public override void Sound()
-//        {
-//            Console.WriteLine("야옹");
-//        }
-//    }
+    class Cat : Animal
+    {
+        public override void Sound()
+        {
+            Console.WriteLine("야옹");
+        }
+    }
 
-//    class Program
-//    {
-//        static public void Main(string[] args)
-//        {
-//            Animal animal = new Animal();
-//            animal.Sound();
-//            animal = new Dog();
-//            animal.Sound();
-//            animal = new Cat();
-//            animal.Sound();
-//        }
-//    }
-//}
+    class Program
+    {
+        static public void Main(string[] args)
+        {
+            Animal animal = new Animal();
+            animal.Sound();
+            animal = new Dog();
+            animal.Sound();
+            animal = new Cat();
+            animal.Sound();
+        }
+    }
+}
 
 
 
