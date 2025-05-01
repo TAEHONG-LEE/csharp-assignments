b2e699a383f6f68f363f8387223a6dcb0098f100
ekfzha <151455218+ekfzha@users.noreply.github.com>
2025-05-01 19:19:22 +0900

    Update Part3_Q5.cs

--- Part3_Q5.cs
+++ Part3_Q5.cs
@@ -1,38 +1,38 @@
-﻿//// See https://aka.ms/new-console-template for more information
-//using System;
+// See https://aka.ms/new-console-template for more information
+using System;
 
-//using System;
+using System;
 
-//namespace basic
-//{ //part3 - Q5
-//    class Part3_Q5
-//    {
-//        static void Print()
-//        {
-//            Console.WriteLine("HelloWorld");
-//        }
+namespace basic
+{ //part3 - Q5
+    class Part3_Q5
+    {
+        static void Print()
+        {
+            Console.WriteLine("HelloWorld");
+        }
 
-//        static void Print(string message)
-//        {
-//            Console.WriteLine(message);
-//        }
+        static void Print(string message)
+        {
+            Console.WriteLine(message);
+        }
 
-//        static void Print(int Count, string message)
-//        {
-//            for (int i = 0; i < Count; i++)
-//            {
-//                Console.WriteLine(message);
-//            }
-//        }
+        static void Print(int Count, string message)
+        {
+            for (int i = 0; i < Count; i++)
+            {
+                Console.WriteLine(message);
+            }
+        }
 
-//        static void Main(string[] args)
-//        {
-//            Print();
-//            Print("HelloWorld");
-//            Print(3, "HelloWorld");
-//        }
-//    }
+        static void Main(string[] args)
+        {
+            Print();
+            Print("HelloWorld");
+            Print(3, "HelloWorld");
+        }
+    }
 
 
 
-//}
+}
